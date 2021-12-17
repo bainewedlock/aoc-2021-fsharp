983f287f6290ca0aadfa18c1c191e0cc2ffda2b2
boeckwi <w.boeck@walz.de>
2021-12-17 11:40:52 +0100

    day 17 part 1

--- aoc-2021-17/Program.fs
+++ aoc-2021-17/Program.fs
@@ -7,7 +7,10 @@ let main argv =
 
     //solve input
     //|> printfn "solution 1: %A"
-    //printfn "(accepted answer: ?)"
+    printfn "(accepted answer: 7626)"
+    // solved with excel:
+    // step 1: find the maximum x velocity which stabilizes inside the area
+    // step 2: find maximum y velocity between 1-200 which hits in the area
 
     //solve2 input
     //|> printfn "solution 2: %A"
