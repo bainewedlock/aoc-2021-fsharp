f4c298d875e73195830fb669a9c93fce9a5379c2
boeckwi <w.boeck@walz.de>
2021-12-20 09:18:29 +0100

    day 20 part 1

--- aoc-2021-20/Solution.fs
+++ aoc-2021-20/Solution.fs
@@ -1,2 +1,95 @@
 module Solution
 
+open System
+
+type Point = int * int
+
+type Image = {
+    bits : Map<int, int>
+    cells: Point Set
+    isInverted : bool
+}
+
+let charToBit = function
+    | '#' -> 1
+    | '.' -> 0
+
+[<RequireQualifiedAccess>]
+module Image =
+    let countCells input =
+        if input.isInverted then failwith "counting cells on inverted image"
+        input.cells.Count
+    let containsPixel p input =
+        if input.isInverted then
+            if input.cells.Contains p then false
+            else true
+        else
+            if input.cells.Contains p then true
+            else false
+
+let parse (input:string) =
+    let lines = input.Split "\r\n" |> Array.toList
+    {
+        isInverted = false
+        bits =
+            lines.[0]
+            |> Seq.indexed
+            |> Seq.map (fun (i,c) ->
+                i, charToBit c)
+            |> Map
+        cells =
+            lines
+            |> List.skip 2
+            |> List.indexed
+            |> List.collect (fun (y,line) -> 
+                line
+                |> Seq.toList
+                |> List.indexed
+                |> List.choose (fun (x, c) ->
+                    if (c = '#') then Some (x,y) else None))
+            |> Set
+    }
+
+let boolToBit = function
+    | false -> 0
+    | true  -> 1
+
+let enhancePoint (x0,y0) input =
+    let bit =
+        [ for i in 0..8 do yield (x0-1+i%3, y0-1+i/3) ]
+        |> List.map (fun p -> Image.containsPixel p input)
+        |> List.map (boolToBit >> string)
+        |> String.concat ""
+        |> fun s -> Convert.ToInt32(s, 2)
+    input.bits.Item bit
+
+
+let findMinMax : int seq -> int * int =
+    Seq.fold (fun (lo,hi) x -> (min x lo, max x hi)) (+99999, -99999)
+
+let enhanceImage input =
+    let (xl,xh) = findMinMax (Set.map fst input.cells)
+    let (yl,yh) = findMinMax (Set.map snd input.cells)
+
+    let invert, check =
+        match input.isInverted, input.bits.Item 0, input.bits.Item 511 with
+        | false, 1, _ -> true, 0
+        | true,  _, 0 -> false, 1
+        | _        -> false, 1
+
+    let newCells = [
+        for y in yl-1..yh+1 do
+        for x in xl-1..xh+1 do
+            if enhancePoint (x,y) input = check then yield (x,y) ]
+
+
+    { input with cells = Set newCells
+                 isInverted = invert }
+
+let solve input =
+    input
+    |> parse
+    |> enhanceImage
+    |> enhanceImage
+    |> fun s -> s.cells.Count
+
--- aoc-2021-20/Tests.fs
+++ aoc-2021-20/Tests.fs
@@ -6,13 +6,49 @@ open Expecto
 
 open Solution
 open Demoinput
+open System
 
 [<Tests>]
 let all =
     testList "all" [
-        testList "foo" [
-            test "bar" {
-                1 =! 1
+        testList "parsing" [
+            test "demoinput" {
+                let image = parse demoinput
+                image.bits.Count =! 512
+                image.bits.Item 0 =! 0
+                image.bits.Item 2 =! 1
+                image.bits.Item 511 =! 1
+
+                Image.countCells image =! 10
+                Image.containsPixel (0,0) image =! true
+                Image.containsPixel (1,0) image =! false
+                Image.containsPixel (4,4) image =! true
+            }
+        ]
+        testList "enhancing" [
+            test "single point" {
+                let input = parse demoinput
+                enhancePoint (2,2) input =! 1
+            }
+            test "whole image" {
+                let image = demoinput |> parse |> enhanceImage
+                Image.containsPixel (-1,-1) image =! false
+                Image.containsPixel ( 0,-1) image =! true
+                Image.containsPixel ( 1,-1) image =! true
+                Image.containsPixel ( 2,-1) image =! false
+                Image.countCells image =! 24
+                Image.containsPixel (-2,-2) image =! false
+            }
+            test "whole image with infinite pixels getting #" {
+                let image =
+                    demoinput
+                    |> parse
+                    |> fun x -> { x with bits = x.bits.Add(0,1) }
+                    |> enhanceImage
+                Image.containsPixel (-2,-2) image =! true
+            }
+            test "solve demoinput" {
+                solve demoinput =! 35
             }
         ]
     ]
