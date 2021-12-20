830c250c5a251d8287b795e512f06fb989a91e6e
boeckwi <w.boeck@walz.de>
2021-12-20 07:22:13 +0100

    day 19 started (gave up for now)

--- aoc-2021-19/Solution.fs
+++ aoc-2021-19/Solution.fs
@@ -1,2 +1,121 @@
 module Solution
 
+//    |1 0 0| |x|   |x*1 + y*0 + z*0|   |x'|
+//    |0 1 0| |y| = |x*0 + y*1 + z*0| = |y'|
+//    |0 0 1| |z|   |x*0 + y*0 + z*1|   |z'|
+let apply (matrix:int List List) (vector: int List) = [
+    for line in 0..2 do
+        let a = vector.[0] * matrix.[line].[0]
+        let b = vector.[1] * matrix.[line].[1]
+        let c = vector.[2] * matrix.[line].[2]
+        yield a+b+c ]
+
+let rotations = [
+    [[1;0;0]
+     [0;1;0]
+     [0;0;1]];
+    [[1;0;0]
+     [0;0;-1]
+     [0;1;0]];
+    [[1;0;0]
+     [0;-1;0]
+     [0;0;-1]];
+    [[1;0;0]
+     [0;0;1]
+     [0;-1;0]];
+    [[0;-1;0]
+     [1;0;0]
+     [0;0;1]];
+    [[0;0;1]
+     [1;0;0]
+     [0;1;0]];
+    [[0;1;0]
+     [1;0;0]
+     [0;0;-1]];
+    [[0;0;-1]
+     [1;0;0]
+     [0;-1;0]];
+    [[-1;0;0 ]
+     [0;-1;0 ]
+     [0;0;1 ]];
+    [[-1;0;0 ]
+     [0;0;-1 ]
+     [0;-1;0 ]];
+    [[-1;0;0 ]
+     [0;1;0 ]
+     [0;0;-1 ]];
+    [[-1;0;0 ]
+     [0;0;1 ]
+     [0;1;0 ]];
+    [[0;1;0 ]
+     [-1;0;0 ]
+     [0;0;1 ]];
+    [[0;0;1 ]
+     [-1;0;0 ]
+     [0;-1;0 ]];
+    [[0;-1;0 ]
+     [-1;0;0 ]
+     [0;0;-1 ]];
+    [[0;0;-1 ]
+     [-1;0;0 ]
+     [0;1;0 ]];
+    [[0;0;-1 ]
+     [0;1;0 ]
+     [1;0;0 ]];
+    [[0;1;0 ]
+     [0;0;1 ]
+     [1;0;0 ]]
+    [[0;0;1 ]
+     [0;-1;0 ]
+     [1;0;0 ]]
+    [[0;-1;0 ]
+     [0;0;-1 ]
+     [1;0;0 ]]
+    [[0;0;-1 ]
+     [0;-1;0 ]
+     [-1;0;0 ]]
+    [[0;-1;0 ]
+     [0;0;1 ]
+     [-1;0;0 ]]
+    [[0;0;1 ]
+     [0;1;0 ]
+     [-1;0;0 ]];
+    [[0;1;0 ]
+     [0;0;-1 ]
+     [-1;0;0]] ]
+
+let parse (input:string) =
+    let parseLine (line:string) =
+        line.Split ","
+        |> Array.toList
+        |> List.map int
+    let folder (scanner::rest) (line:string) =
+        if line.Trim() = ""
+        then []::scanner::rest
+        else (parseLine line::scanner)::rest
+    input.Split "\r\n"
+    |> Seq.filter (fun l -> l.Contains "scanner" |> not)
+    |> Seq.rev
+    |> Seq.fold folder [[]]
+
+type Point = int list
+type Scanner = Point list
+
+let rotateVectors vs m = vs |> List.map (apply m)
+
+// combinations: https://stackoverflow.com/questions/1222185/most-elegant-combinations-of-elements-in-f
+let rec comb n l = 
+    match n, l with
+    | 0, _ -> [[]]
+    | _, [] -> []
+    | k, (x::xs) -> List.map ((@) [x]) (comb (k-1) xs) @ comb k xs
+
+let overlap (ps1:Point list) (ps2:Point list) =
+    []
+
+
+let overlapScanners (s1:Scanner) (s2:Scanner) =
+    rotations
+    |> List.map (rotateVectors s2)
+    |> List.map (fun s2' -> Set.intersect (Set s1) (Set s2'))
+    |> List.maxBy Set.count
--- aoc-2021-19/Tests.fs
+++ aoc-2021-19/Tests.fs
@@ -10,9 +10,60 @@ open Demoinput
 [<Tests>]
 let all =
     testList "all" [
-        testList "foo" [
-            test "bar" {
-                1 =! 1
+        testList "orientation" [
+            test "rotate vector" {
+                let v = [1;2;3]
+                let m = [
+                    [1;0;0]
+                    [0;1;0]
+                    [0;0;1]]
+                apply m v =! [1;2;3]
+            }
+            test "rotate vectors" {
+                let vs = [
+                   [-1;-1;1]
+                   [-2;-2;2]
+                   [-3;-3;3]
+                   [-2;-3;1]
+                   [5;6;-4]
+                   [8;0;7 ] ]
+                let m = [
+                    [-1;0;0]
+                    [0;0;-1]
+                    [0;-1;0]]
+                let vs2 = rotateVectors vs m
+                vs2.[0] =! [1;-1;1]
+                vs2.[5] =! [-8;-7;0]
+                vs2 =! [
+                    [ 1;-1;1 ]
+                    [ 2;-2;2 ]
+                    [ 3;-3;3 ]
+                    [ 2;-1;3 ]
+                    [ -5;4;-6 ]
+                    [ -8;-7;0 ] ]
+            }
+        ]
+        testList "parsing" [
+            test "parse demoinput" {
+                let scanners = parse demoinput
+                scanners.Length =! 5
+                scanners.[0].Length =! 25
+                scanners.[0].[0] =! [404;-588;-901]
+                scanners.[4].Length =! 26
+                scanners.[4].[25] =! [30;-46;-14]
+            }
+        ]
+        testList "overlapping" [
+            test "overlap points" {
+                let ps1 = [[0;2;0];[4;1;0];[3;3;0]]
+                let ps2 = [[-1;-1;0];[-5;0;0];[-2;1;0]]
+                let result = overlap ps1 ps2
+                result.Length =! 3
+            }
+            test "demoinput" {
+                let scanners = parse demoinput
+                let o = overlapScanners scanners.[0] scanners.[1]
+                o.Count =! 12
             }
         ]
     ]
