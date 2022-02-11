d66908179693f7d95c4018c28fd3309ec3c4a514
boeckwi <w.boeck@walz.de>
2022-02-11 09:38:37 +0100

    what

--- aoc-2021-23/Solution.fs
+++ aoc-2021-23/Solution.fs
@@ -6,80 +6,10 @@ type State = char list list
 type Cost = int
 type Decision = int * State
 
-let parseInput (input:string) =
+let compact (input:string) =
     let ls = input.Replace("_", "").Split "\r\n"
     let p i = sprintf "%c%c" ls.[2].[i] ls.[3].[i]
     sprintf "||%s|%s|%s|%s||" (p 3) (p 5) (p 7) (p 9)
 
-let parseState (s:string) =
-    s.Split "|"
-    |> Array.toList
-    |> List.map (Seq.toList)
-
-let printState (state:State) =
-    state
-    |> List.map (Array.ofSeq >> String)
-    |> String.concat "|"
-
-let updateAt vi v xs = [
-    for i,x in List.indexed xs do
-        yield if i=vi then v else x ]
-
-let updateElement key f st = 
-  st |> List.map (fun (k, v) -> if k = key then k, f v else k, v)
-
-let siderooms = Map [
-    2, 'A'
-    4, 'B'
-    6, 'C'
-    8, 'D' ]
-
-let isSideroom = siderooms.ContainsKey
-
-let costFor = function
-    | 'A' -> 1
-    | 'B' -> 10
-    | 'C' -> 100
-    | 'D' -> 1000
-
-let checkMove room pod x =
-    let others = List.filter ((<>)pod) room
-    if Map.tryFind x siderooms = Some pod && others = [] then
-        true, true
-    else if siderooms.ContainsKey x then
-        false, true
-    else if room = [] then
-        true, true
-    else
-        false, false
-
-let allMovesFor dir i (s:string) =
-    let state = parseState s
-    if state.[i] = [] then [] else
-    let pod::rest = state.[i]
-    let moveTo x =
-        state
-        |> updateAt i rest
-        |> updateAt x (pod::state.[x])
-    let rec loop cost x = [
-        if x + dir < 0 then () else
-        if x + dir = state.Length then () else
-        let x = x + dir
-        let canEnter, canPass = checkMove state.[x] pod x
-        if canEnter then
-            let dc = if isSideroom x then 2 else 1
-            yield printState (moveTo x), (cost+dc) * (costFor pod)
-        if canPass then yield! loop (cost+1) x
-    ]
-    let initialcost = if isSideroom i then 1 else 0
-    loop initialcost i
-
-let allRightMovesFor i (s:string) = allMovesFor 1 i s
-let allLeftMovesFor i (s:string) = allMovesFor -1 i s
-
-let options (start:string) =
-    [
-        for i in 0..10 do
-            yield! allLeftMovesFor i start
-            yield! allRightMovesFor i start
-    ]
+let expand (line:string) =
+    ""
--- aoc-2021-23/Tests.fs
+++ aoc-2021-23/Tests.fs
@@ -13,79 +13,66 @@ let dump o = sprintf "%A" o
 let all =
     testList "all" [
         testList "part 1" [
-            test "example" {
-                let input = parseInput demoinput
-                input =! "||BA|CD|BC|DA||"
+            test "compact state" {
+                compact demoinput =! ".|.|BA|.|CD|.|BC|.|DA|.|."
             }
-            test "options" {
-                let result = options "||BA||CD||BC||DA||" |> Map
-                result.Item "B||A||CD||BC||DA||" =! 30
-                result.Item "|B|A||CD||BC||DA||" =! 20
-                result.Item "D||BA||CD||BC||A||" =! 9000
-                result.Item "||BA||CD||BC||A||D" =! 3000
-                result.Item "||BA||D||BC||DA|C|" =! 600
-                result.Count =! 28 // regression test
+            test "expand state" {
+                expand ".|.|BA|.|CD|.|BC|.|DA|.|." =! demoinput
             }
-            test "parse state" {
-                parseState "||BA||CD" =! [[];[];['B';'A'];[];['C';'D']]
-            }
-            test "print state" {
-                printState [[];[];['B';'A'];[];['C';'D']] =! "||BA||CD"
-            }
-            test "pod at 1 (corridor) can move 1 to the left" {
-                let result = "|A" |> allLeftMovesFor 1 |> Set
-                result.Contains ( "A|", 1 ) =! true
-            }
-            test "pod at 2 (sideroom) can move 1-2 to the left" {
-                let result = "||A" |> allLeftMovesFor 2 |> Set
-                result.Contains ( "A||", 3 ) =! true
-                result.Contains ( "|A|", 2 ) =! true
-            }
-            test "pod cant move to left if corridor blocked" {
-                let result = "B|A" |> allLeftMovesFor 1 |> Set
-                result.Count =! 0
-                let result = "B||A" |> allLeftMovesFor 2 |> Set
-                result.Count =! 1
-            }
-            test "pod can move left to/over sideroom if empty" {
-                let result = "|||A" |> allLeftMovesFor 3 |> Set
-                result.Contains ( "||A|", 2 ) =! true
-                result.Contains ( "|A||", 2 ) =! true
-                result.Contains ( "A|||", 3 ) =! true
-                result.Count =! 3
-            }
-            test "pod can move left to target sideroom" {
-                let result = "|||A" |> allLeftMovesFor 3 |> Set
-                result.Contains ( "||A|", 2 ) =! true
-            }
-            test "pod cant move left to wrong sideroom" {
-                let result = "|||B" |> allLeftMovesFor 3 |> Map
-                result.ContainsKey "||B|" =! false
-            }
-            test "pod can move right to/over sideroom if empty" {
-                let result = "||B|||" |> allRightMovesFor 2 |> Set
-                result.Contains ( "|||B||", 20 ) =! true
-                result.Contains ( "||||B|", 40 ) =! true
-                result.Contains ( "|||||B", 40 ) =! true
-                result.Count =! 3
-            }
-            test "no left moves if no pod" {
-                let result = "A||C" |> allLeftMovesFor 1 |> Set
-                result.Count =! 0
-            }
-            test "example step 1 is found" {
-                let xs = options "||BA||CD||BC||DA||" |> Set
-                Expect.contains xs ( "||BA|B|CD||C||DA||", 40 ) ""
-            }
-            test "example step 2 is found" {
-                let xs = options "||BA|B|CD||C||DA||" |> Set
-                Expect.contains xs ( "||BA|B|D||CC||DA||", 400 ) ""
-            }
-            test "example step 3 is found" {
-                let xs = options "||BA|B|D||CC||DA||" |> Set
-                Expect.contains xs ( "||BA|B||D|CC||DA||", 3000 ) ""
-                // fails because D needs to take two steps out of the sideroom ^_^ bad model d'oh
-            }
+            //test "pod at 1 (corridor) can move 1 to the left" {
+            //    let result = "|A" |> allLeftMovesFor 1 |> Set
+            //    result.Contains ( "A|", 1 ) =! true
+            //}
+            //test "pod at 2 (sideroom) can move 1-2 to the left" {
+            //    let result = "||A" |> allLeftMovesFor 2 |> Set
+            //    result.Contains ( "A||", 3 ) =! true
+            //    result.Contains ( "|A|", 2 ) =! true
+            //}
+            //test "pod cant move to left if corridor blocked" {
+            //    let result = "B|A" |> allLeftMovesFor 1 |> Set
+            //    result.Count =! 0
+            //    let result = "B||A" |> allLeftMovesFor 2 |> Set
+            //    result.Count =! 1
+            //}
+            //test "pod can move left to/over sideroom if empty" {
+            //    let result = "|||A" |> allLeftMovesFor 3 |> Set
+            //    result.Contains ( "||A|", 2 ) =! true
+            //    result.Contains ( "|A||", 2 ) =! true
+            //    result.Contains ( "A|||", 3 ) =! true
+            //    result.Count =! 3
+            //}
+            //test "pod can move left to target sideroom" {
+            //    let result = "|||A" |> allLeftMovesFor 3 |> Set
+            //    result.Contains ( "||A|", 2 ) =! true
+            //}
+            //test "pod cant move left to wrong sideroom" {
+            //    let result = "|||B" |> allLeftMovesFor 3 |> Map
+            //    result.ContainsKey "||B|" =! false
+            //}
+            //test "pod can move right to/over sideroom if empty" {
+            //    let result = "||B|||" |> allRightMovesFor 2 |> Set
+            //    result.Contains ( "|||B||", 20 ) =! true
+            //    result.Contains ( "||||B|", 40 ) =! true
+            //    result.Contains ( "|||||B", 40 ) =! true
+            //    result.Count =! 3
+            //}
+            //test "no left moves if no pod" {
+            //    let result = "A||C" |> allLeftMovesFor 1 |> Set
+            //    result.Count =! 0
+            //}
+            //test "example step 1 is found" {
+            //    let xs = options     "||BA||CD||BC||DA||" |> Set
+            //    Expect.contains xs ( "||BA|B|CD||C||DA||", 40 ) ""
+            //}
+            //test "example step 2 is found" {
+            //    let xs = options     "||BA|B|CD||C||DA||" |> Set
+            //    Expect.contains xs ( "||BA|B|D||CC||DA||", 400 ) ""
+            //}
+            //test "example step 3 is found" {
+            //    let xs = options     "||BA|B|D||CC||DA||" |> Set
+            //    Expect.contains xs ( "||BA|B||D|CC||DA||", 3000 ) ""
+            //    // fails because D needs to take two steps out of the sideroom : bad model!
+            //}
         ]
     ]
 
